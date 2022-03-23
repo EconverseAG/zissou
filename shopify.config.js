108217fb7c4dfd69a1a29209ff47654a8a107868
Pedro Lucas <pedro.lucas@econverse.com.br>
2022-03-23 13:51:35 -0300

    eslint

--- shopify.config.js
+++ shopify.config.js
@@ -1,5 +1,5 @@
 export default {
   storeDomain: 'zissou.myshopify.com',
   storefrontToken: 'bcc7ee1f7523fb44f93be010e0bb5120',
-  storefrontApiVersion: '2022-01'
+  storefrontApiVersion: '2022-01',
 };
