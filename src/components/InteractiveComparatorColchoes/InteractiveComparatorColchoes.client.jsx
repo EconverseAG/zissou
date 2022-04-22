bdafdea65ba4f831e37a484e4ad645039749bc3c
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-04-22 14:49:14 -0300

    feat(base): responsive interactive comparator

--- src/components/InteractiveComparatorColchoes/InteractiveComparatorColchoes.client.jsx
+++ src/components/InteractiveComparatorColchoes/InteractiveComparatorColchoes.client.jsx
@@ -4,10 +4,13 @@ import {Link} from '@shopify/hydrogen/client';
 
 import * as styles from './InteractiveComparatorColchoes.module.scss';
 import SliderController from '../../assets/slider-controller.svg';
+import useMobile from '../../hooks/useMobile';
 
 export default function InteractiveComparatorColchoes() {
   const [sliderPercent, setSliderPercent] = useState(50);
   const [isHolding, setIsHolding] = useState(false);
+
+  const {isMobile} = useMobile();
 
   const handleMouseMove = useCallback(
     (e) => {
@@ -36,10 +39,16 @@ export default function InteractiveComparatorColchoes() {
 
   return (
     <div
-      className={styles.wrapper}
+      className={`${styles.wrapper} ${isMobile ? styles.mobile : ''}`}
       onMouseMove={handleMouseMove}
       onTouchMove={handleMouseMove}
     >
+      <div
+        className={styles.colchaoCoral}
+        style={{
+          clipPath: `polygon(0% 0%,${sliderPercent}% 0%,${sliderPercent}% 100%, 0% 100%)`,
+        }}
+      />
       <div className={styles.slider} style={{width: `${sliderPercent}%`}}>
         <button
           type="button"
