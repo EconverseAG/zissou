9f52b80d76c47d8d0ff89f8e647a24e839b251d9
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-04-22 17:00:37 -0300

    feat(base): responsive potencialize sleep section

--- src/components/PotencializeSleep/PotencializeSleep.client.jsx
+++ src/components/PotencializeSleep/PotencializeSleep.client.jsx
@@ -1,11 +1,15 @@
 import {Image} from '@shopify/hydrogen';
 import * as styles from './PotencializeSleep.module.scss';
 
+import useMobile from '../../hooks/useMobile';
+
 import gif from '../../assets/potencialize-sleep.gif';
 
 export default function PotencializeSleep() {
+  const {isMobile} = useMobile();
+
   return (
-    <div className={styles.wrapper}>
+    <div className={`${styles.wrapper} ${isMobile ? styles.mobile : ''}`}>
       <h2>
         Pensada para{' '}
         <strong>potencializar ao máximo a experiência de sono</strong> dos
