d8262e791bb7ead7eae901457152f3f01a75fdf6
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-04-22 14:56:58 -0300

    feat(base): responsive base banner

--- src/components/BaseBanner/BaseBanner.client.jsx
+++ src/components/BaseBanner/BaseBanner.client.jsx
@@ -1,8 +1,11 @@
+import useMobile from '../../hooks/useMobile';
 import * as styles from './BaseBanner.module.scss';
 
 export default function BaseBanner() {
+  const {isMobile} = useMobile();
+
   return (
-    <div className={styles.wrapper}>
+    <div className={`${styles.wrapper} ${isMobile ? styles.mobile : ''}`}>
       <div className={styles.content}>
         <strong>Base Zissou</strong>
         <span>Estabilidade para dormir e viver</span>
