d706b51fb7e9bec6059398423035a2bd7663fc3f
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-04-23 16:35:04 -0300

    feat(travesseiro): try it for 100 days, zissou stores and partners sections

--- src/components/Product/Travesseiro.client.jsx
+++ src/components/Product/Travesseiro.client.jsx
@@ -1,11 +1,17 @@
 import HowDoYouSleep from '../HowDoYouSleep/HowDoYouSleep.client';
 import ProductSection from './ProductSection.client';
+import TryItFor100Days from '../TryItFor100Days/TryItFor100Days.client';
+import ZissouStores from '../ZissouStores/ZissouStores.client';
+import Partners from '../Partners/Partners.client';
 
 export default function Travesseiro({product}) {
   return (
     <div className="flex flex-col">
       <ProductSection product={product} />
       <HowDoYouSleep />
+      <TryItFor100Days />
+      <ZissouStores />
+      <Partners color="#779ABF" />
     </div>
   );
 }
