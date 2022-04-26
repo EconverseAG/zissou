6c523b651fddd7d22bac1bdd59855183ba3a6d91
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-04-26 17:16:38 -0300

    feat(duvet): add default sections

--- src/components/Product/Duvet.client.jsx
+++ src/components/Product/Duvet.client.jsx
@@ -1,11 +1,17 @@
 import DuvetBanner from '../DuvetBanner';
+import Partners from '../Partners/Partners.client';
 import TheSecret from '../TheSecret';
+import TryItFor100Days from '../TryItFor100Days/TryItFor100Days.client';
+import ZissouStores from '../ZissouStores/ZissouStores.client';
 
 export default function Duvet() {
   return (
     <div className="flex flex-col">
       <DuvetBanner />
       <TheSecret productName="Duvet Zissou" />
+      <TryItFor100Days />
+      <ZissouStores />
+      <Partners color="#779ABF" />
     </div>
   );
 }
