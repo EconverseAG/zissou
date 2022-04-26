0c73ec5a8125f970baed67af501fad2bbc44e7c6
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-04-26 17:09:05 -0300

    refactor: the secret section generalist with productName prop

--- src/components/Product/Duvet.client.jsx
+++ src/components/Product/Duvet.client.jsx
@@ -1,9 +1,11 @@
 import DuvetBanner from '../DuvetBanner';
+import TheSecret from '../TheSecret';
 
 export default function Duvet() {
   return (
     <div className="flex flex-col">
       <DuvetBanner />
+      <TheSecret productName="Duvet Zissou" />
     </div>
   );
 }
