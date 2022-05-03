509ccee49056689972f956e899577cc099e527c3
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-05-03 14:29:09 -0300

    chore: remove ProductSection references

--- src/components/Product/Colchao.client.jsx
+++ src/components/Product/Colchao.client.jsx
@@ -13,7 +13,6 @@ import TryItFor100Days from '../TryItFor100Days/TryItFor100Days.client';
 import DoubtsProductPage from '../ProductPage/DoubtsProductPage/DoubtsProductPage.client';
 import InstagramZissou from '../../components/InstagramZissou/InstagramZissou.client';
 import SpecsColchaoSlick from '../ProductPage/SpecsColchaoSlick/SpecsColchaoSlick.client';
-// import ProductSection from './ProductSection.client';
 import TourColchao from '../ProductPage/TourColchao/TourColchao.client';
 import CoralProduct from '../CoralProduct';
 
