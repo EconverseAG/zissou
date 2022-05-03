332fb6a56931c68061ca180ed07b65c408fa2ec8
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-05-03 15:08:28 -0300

    refactor(BaseProduct): ProductSection components structure

--- src/components/BaseProduct/BaseProduct.client.jsx
+++ src/components/BaseProduct/BaseProduct.client.jsx
@@ -3,6 +3,9 @@ import ZissouProductOptions from '../ZissouProductOptions';
 import ZissouProductImages from '../ZissouProductImages';
 import ZissouProductPrice from '../ZissouProductPrice';
 import WhatsAppBanner from '../WhatsAppBanner';
+import ProductSection from '../ProductSection';
+import ProductSectionRight from '../ProductSection/ProductSectionRight';
+import ProductSectionLeft from '../ProductSection/ProductSectionLeft';
 
 import useMobile from '../../hooks/useMobile';
 
@@ -12,19 +15,23 @@ function BaseProduct() {
   const {isMobile} = useMobile();
 
   return (
-    <div className={`${styles.wrapper} ${isMobile ? styles.mobile : ''}`}>
-      <ZissouProductImages title="Estilosa, resistente e perfeita" thumbs />
-      <div className={styles.productInfo}>
+    <ProductSection
+      className={`${styles.wrapper} ${isMobile ? styles.mobile : ''}`}
+    >
+      <ProductSectionLeft>
+        <ZissouProductImages title="Estilosa, resistente e perfeita" thumbs />
+      </ProductSectionLeft>
+      <ProductSectionRight>
         <ZissouProductOptions
           title="Selecione o tamanho da sua base:"
           uppercaseTitle={isMobile}
           icons
         />
-        <ZissouProductPrice className={styles.productInfoPrice} />
-        <ZissouAddToCart className={styles.productInfoAddToCart} />
-        <WhatsAppBanner className={styles.productInfoWhatsApp} />
-      </div>
-    </div>
+        <ZissouProductPrice className={styles.Price} />
+        <ZissouAddToCart className={styles.AddToCart} />
+        <WhatsAppBanner className={styles.WhatsApp} />
+      </ProductSectionRight>
+    </ProductSection>
   );
 }
 
