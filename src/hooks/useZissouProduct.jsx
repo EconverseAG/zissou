e68f5db90e78cde1a0452a3c867920449575d8c5
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-05-13 11:04:20 -0300

    fix(useZissouProduct): remove commented element

--- src/hooks/useZissouProduct.jsx
+++ src/hooks/useZissouProduct.jsx
@@ -125,7 +125,6 @@ function ZissouProductProvider({
     >
       <ProductProvider data={product}>
         <BuyTogetherProvider>{children}</BuyTogetherProvider>
-        {/* {children} */}
       </ProductProvider>
     </ZissouProductContext.Provider>
   );
