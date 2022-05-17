5388dbf2a347bff7dd1e531bc18ec8fc6ecbac5f
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-05-17 16:43:45 -0300

    feat(TableCheck): responsive image sizes

--- src/components/ComparativoColchoes/TableCheck/TableCheck.client.jsx
+++ src/components/ComparativoColchoes/TableCheck/TableCheck.client.jsx
@@ -1,20 +1,32 @@
 import {Image} from '@shopify/hydrogen';
 
+import useMobile from '../../../hooks/useMobile';
+
 import Check from '../../../assets/check-icon.png';
 import CheckBlue from '../../../assets/check-blue.png';
 import Uncheck from '../../../assets/uncheck.png';
 
 function TableCheck({checked, colorBlue}) {
+  const {isMobile} = useMobile();
+
   return (
     <>
       {checked ? (
         colorBlue ? (
-          <Image src={CheckBlue} width={40} height={37} />
+          <Image
+            src={CheckBlue}
+            width={isMobile ? 18 : 40}
+            height={isMobile ? 16 : 37}
+          />
         ) : (
-          <Image src={Check} width={40} height={37} />
+          <Image
+            src={Check}
+            width={isMobile ? 18 : 40}
+            height={isMobile ? 16 : 37}
+          />
         )
       ) : (
-        <Image src={Uncheck} width={59} height={3} />
+        <Image src={Uncheck} width={isMobile ? 31 : 59} height={3} />
       )}
     </>
   );
