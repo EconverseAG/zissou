481c03aebcec57be4f9d3227af0517446a54459c
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-05-24 10:33:14 -0300

    feat(ProductFixedBar): add scroll interaction

--- src/components/ProductFixedBar/ProductFixedBar.client.jsx
+++ src/components/ProductFixedBar/ProductFixedBar.client.jsx
@@ -1,10 +1,25 @@
+import {useState, useEffect} from 'react';
 import ZissouAddToCart from '../ZissouAddToCart';
 
 import * as styles from './ProductFixedBar.module.scss';
 
 function ProductFixedBar({title}) {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 1028) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+
+    return () => window.removeEventListener('scroll', () => {});
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${show ? styles.show : ''}`}>
       <div className={styles.infos}>
         <h2>{title}</h2>
         <span>Experimente por 100 dias. Um ano de garantia.</span>
