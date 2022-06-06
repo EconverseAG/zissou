e0570e2d056188abd4570127fbb766dae91ec1fb
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-06-06 15:16:40 -0300

    fix(TableCheck): color choice not working

--- src/components/ComparativoColchoes/TableCheck/TableCheck.client.jsx
+++ src/components/ComparativoColchoes/TableCheck/TableCheck.client.jsx
@@ -2,29 +2,37 @@ import {Image} from '@shopify/hydrogen';
 
 import useMobile from '../../../hooks/useMobile';
 
-import Check from '../../../assets/check-icon.svg';
-import CheckBlue from '../../../assets/check-blue.svg';
 import Uncheck from '../../../assets/uncheck.svg';
+import {useMemo} from 'react';
 
-function TableCheck({checked, colorBlue}) {
+function TableCheck({checked, colorBlue, colorSpark}) {
   const {isMobile} = useMobile();
+
+  const color = useMemo(
+    () => (colorBlue ? '#415264' : colorSpark ? '#9873AC' : '#F48580'),
+    [colorBlue, colorSpark],
+  );
 
   return (
     <>
       {checked ? (
-        colorBlue ? (
-          <Image
-            src={CheckBlue}
-            width={isMobile ? 18 : 40}
-            height={isMobile ? 16 : 37}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={isMobile ? 18 : 40}
+          height={isMobile ? 16 : 37}
+          viewBox="0 0 39.373 36.456"
+        >
+          <path
+            id="Caminho_157"
+            data-name="Caminho 157"
+            d="M88.984,1082.04l11.127,10.941,19.819-27.673"
+            transform="translate(-84.742 -1061.123)"
+            fill="none"
+            stroke={color}
+            strokeLinecap="round"
+            strokeWidth="6"
           />
-        ) : (
-          <Image
-            src={Check}
-            width={isMobile ? 18 : 40}
-            height={isMobile ? 16 : 37}
-          />
-        )
+        </svg>
       ) : (
         <Image src={Uncheck} width={isMobile ? 31 : 59} height={3} />
       )}
