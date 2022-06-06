858f2f391627c589526fb6f18d2cc93de859e167
Pedro Lucas <pedro.lucas@econverse.com.br>
2022-06-06 18:17:01 -0300

    Adicionado Spark na home

--- src/components/CoralOrBlue/CoralOrBlue.client.jsx
+++ src/components/CoralOrBlue/CoralOrBlue.client.jsx
@@ -1,7 +1,6 @@
 import {Link} from '@shopify/hydrogen/client';
 import * as styles from './CoralOrBlue.module.scss';
 
-// import Gif from '../../assets/gif_coral_ou_blue.webm';
 import GifFundoMobile from '../../assets/gif_fundo_mobile.webm';
 import GifFundo from '../../assets/gif_coral_ou_blue_fundo.webm';
 import useMobile from '../../hooks/useMobile';
@@ -11,7 +10,7 @@ export default function CoralOrBlue() {
 
   return (
     <div className={styles.CoralOrBlueContainer}>
-      <strong className={styles.CoralOrBlueTitle}>Coral ou blue?</strong>
+      <strong className={styles.CoralOrBlueTitle}>SPARK, CORAL OU BLUE?</strong>
       <video src={isMobile ? GifFundoMobile : GifFundo} autoPlay loop muted />
       <Link to="/pages/colchoes-zissou" className={styles.CoralOrBlueLink}>
         Comparar os colchões
--- src/components/SleepAndLive/SleepAndLive.client.jsx
+++ src/components/SleepAndLive/SleepAndLive.client.jsx
@@ -2,21 +2,58 @@ import {Link, Image} from '@shopify/hydrogen/client';
 
 import * as styles from './SleepAndLive.module.scss';
 
+import LogoSpark from '../../assets/spark-logo.png';
+import ColchaoSpark from '../../assets/colchao_spark.png';
 import ColchaoCoral from '../../assets/colchao_coral.webp';
 import ColchaoBlue from '../../assets/colchao_blue.webp';
+import useMobile from '../../hooks/useMobile';
 
 export default function SleepAndLive() {
+  const {isMobile} = useMobile();
+
   return (
     <div className={styles.SleepAndLiveContainer}>
       <span>Dormir & Viver</span>
       <div className={styles.SleepAndLiveContent}>
-        <div>
-          <p>
+        {!isMobile && (
+          <div className={`${styles.Prod} ${styles.Spark}`}>
+            <p className={styles.Flag}>Novidade</p>
+            <Image
+              src={LogoSpark}
+              width={114}
+              height={37}
+              className={styles.ProdTitle}
+            />
+            <span className={styles.ProdSubtitle}>Onde tudo começa</span>
+            <Link
+              to="/products/colchao-zissou-coral-hibrido-2"
+              className={styles.ProdImage}
+            >
+              <Image
+                src={ColchaoSpark}
+                width="228"
+                height="82"
+                loading={'lazy'}
+              />
+            </Link>
+            <Link
+              to="/products/colchao-zissou-coral-hibrido-2"
+              className={styles.ProdLink}
+            >
+              Colchão Zissou Spark
+            </Link>
+          </div>
+        )}
+        <div className={`${styles.Prod} ${styles.Coral}`}>
+          <p className={styles.Flag}>
             Mais <br /> vendido
           </p>
-          <strong>Coral</strong>
-          <span>A evolução do sono</span>
-          <Link to="/products/colchao-zissou-coral-original">
+          <strong className={styles.ProdTitle}>Coral</strong>
+          <span className={styles.ProdSubtitle}>A evolução do sono</span>
+          <Link
+            to="/products/colchao-zissou-coral-original"
+            className={styles.ProdImage}
+          >
             <Image
               src={ColchaoCoral}
               width="228"
@@ -24,21 +61,55 @@ export default function SleepAndLive() {
               loading={'lazy'}
             />
           </Link>
-          <Link to="/products/colchao-zissou-coral-original">
+          <Link
+            to="/products/colchao-zissou-coral-original"
+            className={styles.ProdLink}
+          >
             Colchão Zissou Coral
           </Link>
         </div>
-        <div>
-          <p>
+        <div className={`${styles.Prod} ${styles.Blue}`}>
+          <p className={styles.Flag}>
             Mais <br /> avançado
           </p>
-          <strong>Blue</strong>
-          <span>Liberte seus sonhos</span>
-          <Link to="/products/colchao-zissou-blue">
+          <strong className={styles.ProdTitle}>Blue</strong>
+          <span className={styles.ProdSubtitle}>Liberte seus sonhos</span>
+          <Link to="/products/colchao-zissou-blue" className={styles.ProdImage}>
             <Image src={ColchaoBlue} width="228" height="82" loading={'lazy'} />
           </Link>
-          <Link to="/products/colchao-zissou-blue">Colchão Zissou Blue</Link>
+          <Link to="/products/colchao-zissou-blue" className={styles.ProdLink}>
+            Colchão Zissou Blue
+          </Link>
         </div>
+        {isMobile && (
+          <div className={`${styles.Prod} ${styles.Spark}`}>
+            <p className={styles.Flag}>Novidade</p>
+            <Image
+              src={LogoSpark}
+              width={114}
+              height={37}
+              className={styles.ProdTitle}
+            />
+            <span className={styles.ProdSubtitle}>Onde tudo começa</span>
+            <Link
+              to="/products/colchao-zissou-coral-hibrido-2"
+              className={styles.ProdImage}
+            >
+              <Image
+                src={ColchaoSpark}
+                width="228"
+                height="82"
+                loading={'lazy'}
+              />
+            </Link>
+            <Link
+              to="/products/colchao-zissou-coral-hibrido-2"
+              className={styles.ProdLink}
+            >
+              Colchão Zissou Spark
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
