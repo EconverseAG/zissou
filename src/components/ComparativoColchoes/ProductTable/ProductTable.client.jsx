49f1d90ba54c4630a9829f697ae2c058014c36d8
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-06-06 11:53:56 -0300

    feat(comparativo): remove escala de firmeza

--- src/components/ComparativoColchoes/ProductTable/ProductTable.client.jsx
+++ src/components/ComparativoColchoes/ProductTable/ProductTable.client.jsx
@@ -8,9 +8,6 @@ import * as styles from './ProductTable.module.scss';
 
 import PhotoCoral from '../../../assets/foto_coral.png';
 import PhotoBlue from '../../../assets/foto_blue.png';
-import Firme from '../../../assets/firme.svg';
-import MedioMacio from '../../../assets/medio-macio.svg';
-import MedioFirme from '../../../assets/medio-firme.svg';
 
 function ProductTable({coral, coralHybrid, blue, className, ...rest}) {
   const {isMobile} = useMobile();
@@ -54,20 +51,6 @@ function ProductTable({coral, coralHybrid, blue, className, ...rest}) {
         <td>25cm</td>
         <td>30cm</td>
         <td>32cm</td>
-      </TableRow>
-      <TableRow heading="Escala de firmeza" className={styles.firmnessScale}>
-        <td>
-          <Image src={Firme} width={259} height={45} />
-          <span>Firme</span>
-        </td>
-        <td>
-          <Image src={MedioMacio} width={259} height={45} />
-          <span>Médio macio</span>
-        </td>
-        <td>
-          <Image src={MedioFirme} width={259} height={45} />
-          <span>Médio firme</span>
-        </td>
       </TableRow>
     </Table>
   );
