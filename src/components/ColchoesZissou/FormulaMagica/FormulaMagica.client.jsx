45839454845ab35e80e6f3f964de919088ac8aaf
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-06-09 11:06:23 -0300

    feat(FormulaMagica): change title and text

--- src/components/ColchoesZissou/FormulaMagica/FormulaMagica.client.jsx
+++ src/components/ColchoesZissou/FormulaMagica/FormulaMagica.client.jsx
@@ -10,13 +10,12 @@ export default function FormulaMagica() {
   return (
     <div className={styles.FormulaMagicaContainer}>
       <strong className={styles.FormulaMagicaTitle}>
-        A <p className={styles.FormulaMagicaTitleOtherColor}>FÓRMULA MÁGICA</p>{' '}
-        DO LABORATÓRIO ZISSOU
+        O LABORATÓRIO ZISSOU
       </strong>
       <span className={styles.FormulaMagicaSubtitle}>
-        Começamos a redefinir a sua relação com o sono antes mesmo de lançarmos
-        nossos produtos. Nosso desenvolvimento é feito junto com vocês aplicando
-        as melhores tecnologias do mercado
+        O desenvolvimento dos produtos Zissou é feito em conjunto com vocês,
+        aplicando as melhores tecnologias do mercado, para redefinir sua relação
+        com o sono
       </span>
       <div className={styles.FormulaMagicaImage}>
         <video
