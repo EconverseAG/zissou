5654998697c8c09d9cc509a057b433f2440cfb8d
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-06-09 11:40:31 -0300

    feat(SpecsCoralSlick): remove technical information

--- src/components/ProductPage/SpecsColchaoSlick/SpecsCoralSlick.client.jsx
+++ src/components/ProductPage/SpecsColchaoSlick/SpecsCoralSlick.client.jsx
@@ -586,20 +586,6 @@ export default function SpecsCoralSlick() {
                   </Link>
                 </span>
               </div>
-              <div className={styles.BeneficiosItem}>
-                <strong className={styles.BeneficiosItemTitle}>
-                  INFORMAÇÕES TÉCNICAS
-                </strong>
-                <span className={styles.BeneficiosItemSubtitle}>
-                  Para saber mais sobre informações técnicas dos colchões,{' '}
-                  <Link
-                    to="/pages/comparativo-colchoes"
-                    className={styles.BeneficiosItemSubtitleLink}
-                  >
-                    clique aqui
-                  </Link>
-                </span>
-              </div>
             </div>
           </div>
           <div
